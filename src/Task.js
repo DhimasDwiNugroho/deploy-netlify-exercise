c7d4ef29ff25a84dec74ae7d39aac27cab913ab8
DhimasDwiNugroho <89631675+DhimasDwiNugroho@users.noreply.github.com>
2023-06-14 20:15:20 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Dhimas Dwi Nugroho" // TODO: replace this
+const studentId = "FE4300715" // TODO: replace this
+
+export { url, studentName, studentId }
